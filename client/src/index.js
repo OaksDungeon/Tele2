5d3aa840ce41b00d27a83cacc9fd570c3fec4a26
Kaiser_Timotheus_Der_Erste <127769312+KaiserVonGIUI@users.noreply.github.com>
2024-05-22 22:45:44 +0300

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Найти целевой элемент для рендеринга
+const container = document.getElementById('root');
+
+// Убедиться, что элемент существует и инициализировать рендеринг
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error('Target container "root" not found');
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
